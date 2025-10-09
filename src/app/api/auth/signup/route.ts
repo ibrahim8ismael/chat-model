a83b133fde4559e4add7b96efa78669e9e31b520
ibrahim <ibrahim.ismael204@gmail.com>
2025-10-10 00:08:48 +0300

    fix versel logs

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -25,7 +25,7 @@ export async function POST(req: Request) {
     });
 
     return NextResponse.json({ user }, { status: 201 });
-  } catch (error) {
+  } catch {
     return NextResponse.json({ message: 'Something went wrong' }, { status: 500 });
   }
 }
